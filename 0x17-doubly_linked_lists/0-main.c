641d96f2adb8b6c928e38d300f37bcebee26ec86
dericko681 <abonghoderick@gmail.com>
2024-01-11 09:43:59 -0800

    task file

---
+++ 0x17-doubly_linked_lists/0-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    dlistint_t *head;
+    dlistint_t *new;
+    dlistint_t hello = {8, NULL, NULL};
+    size_t n;
+
+
---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * f4 - finds the biggest number
+ *
+ * @usrn: username
+ * @len: length of username
+ * Return: the biggest number
+ */
+int f4(char *usrn, int len)
+{
+	int ch;
+	int vch;
+	unsigned int rand_num;
+
+	ch = *usrn;
+	vch = 0;
+
+	while (vch < len)
+	{
+		if (ch < usrn[vch])
+			ch = usrn[vch];
+		vch += 1;
+
